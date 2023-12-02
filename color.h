e84b2926a926195367cc47b59d38175dd4e66ec4
nik <79939309+kaczynski-tech-tips@users.noreply.github.com>
2023-12-01 23:46:36 -0500

    WORKING

--- color.h
+++ color.h
@@ -17,7 +17,10 @@ void write_color(std::ostream &out, color pixel_color, int samples_per_pixel)
     b *= scale;
     
     // writes translated value of each color component
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' ' << static_cast<int>(255.999 * pixel_color.y()) << ' ' << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    static const interval intensity(0.000, 0.999);
+    out << static_cast<int>(256 * intensity.clamp(r)) << ' '
+        << static_cast<int>(256 * intensity.clamp(g)) << ' '
+        << static_cast<int>(256 * intensity.clamp(b)) << '\n';
 }
 
 #endif
